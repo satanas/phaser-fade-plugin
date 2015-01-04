a74c303a87bbc8beb1034fa9e68897c9e6d26cf0
satanas <wil.alejandro@gmail.com>
2015-01-04 16:48:48 -0300

    Added fadeIn effect and refactored code

--- phaser-fade.js
+++ phaser-fade.js
@@ -8,15 +8,27 @@ Phaser.Plugin.Fade.prototype.constructor = Phaser.Plugin.SamplePlugin;
 // HexColor is a number like 0xff0000
 // Delay is the time to wait before calling the callback
 Phaser.Plugin.Fade.prototype.fadeOut = function(hexColor, time, delay, callback) {
+  this.fade(hexColor, time, delay, 0, 1, callback);
+};
+
+// HexColor is a number like 0xff0000
+// Delay is the time to wait before calling the callback
+Phaser.Plugin.Fade.prototype.fadeIn = function(hexColor, time, delay, callback) {
+  this.fade(hexColor, time, delay, 1, 0, callback);
+};
+
+Phaser.Plugin.Fade.prototype.fade = function(hexColor, time, delay, fromAlpha, toAlpha, callback) {
   delay = delay || 0;
   var bg = this.game.add.graphics(0, 0);
   bg.beginFill(hexColor, 1);
   bg.drawRect(this.game.camera.x, this.game.camera.y, this.game.width * 1.5, this.game.height * 1.5);
-  bg.alpha = 0;
+  bg.alpha = fromAlpha;
   bg.endFill();
 
   var s = this.game.add.tween(bg);
-  s.to({alpha: 1}, time, null);
-  s.onComplete.add(callback, this);
+  s.to({alpha: toAlpha}, time, null);
+  if (callback) {
+    s.onComplete.add(callback, this);
+  }
   s.start();
 };
